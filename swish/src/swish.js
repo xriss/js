cfbb798617addccedf2f09271b6c02cf71c7c3fd
kriss <kriss@localhost>
2012-01-19 18:11:23 +0000

    gonna make a swish slideshow thingy

---
+++ swish/src/swish.js
@@ -0,0 +1,19 @@
+
+Swish=function(opts){
+
+	var storage_available=typeof window.localStorage!=='undefined';
+	var json_available=typeof window.JSON!=='undefined';
+	
+	var swish={};
+	
+	
+	swish.setup=function(opts)
+	{
+		
+//		opts.div.css("height","200px");
+		
+	};
+
+		
+	return swish;
+};
