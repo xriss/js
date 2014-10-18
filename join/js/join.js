2cb1155ab2a66c3f5e22ba058e2db62c09b6fdd9
Kriss <kriss@XIXs.com>
2014-10-18 23:17:36 +0100

    template setup

--- join/js/join.js
+++ join/js/join.js
@@ -5,7 +5,28 @@ exports.setup=function(opts){
 
 	var join={opts:opts};
 
-//	require('./join.html.js').setup(join);			
+//	require('./join.html.js').setup(join);
+
+	join.template=$("<div></div>");
+		
+	join.fill=function(){
+		opts.div.empty().append( join.template.find(".wetjoin_main").clone() );
+		join.page("login")
+	};
+
+	join.page=function(pagename){
+		$(".wetjoin_main .wetjoin_page").empty().append( join.template.find(".wetjoin_page_"+pagename).clone() );
+		join.bind();
+	};
+
+	join.bind=function(){
+		$(".wetjoin_main .wetjoin_header_join").off("click").on("click",function(){join.page("join");});
+		$(".wetjoin_main .wetjoin_header_login").off("click").on("click",function(){join.page("login");});
+		$(".wetjoin_main .wetjoin_header_forgot").off("click").on("click",function(){join.page("forgot");});
+	};
+	
+
+	join.template.load("template.html",join.fill);
 
 	return join;
 
