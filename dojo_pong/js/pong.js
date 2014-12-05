3ff531e5d9dabb9c0e4cc45df0237cf07e6ed7bb
kriss <kriss@localhost>
2014-12-05 08:56:40 +0000

    dojo  test

---
+++ dojo_pong/js/pong.js
@@ -0,0 +1,14 @@
+
+var ls=function(a) { console.log(util.inspect(a,{depth:null})); }
+
+exports.setup=function(opts){
+
+	var pong={opts:opts};
+	
+	var $=require("./jquery.js");
+	
+	
+	
+	
+	return pong;
+};
